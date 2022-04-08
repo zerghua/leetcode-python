7bda88dbddbd9d30e61df721e36407decf553995
hua_mac <zerghua@gmail.com>
2022-04-08 11:38:24 -0400

    N1331,1332,1337,1342 easy

---
+++ N1337_TheKWeakestRowsinaMatrix.py
@@ -0,0 +1,90 @@
+#
+# Create by Hua on 4/8/22.
+#
+
+"""
+You are given an m x n binary matrix mat of 1's (representing soldiers) and 0's (representing civilians). The soldiers are positioned in front of the civilians. That is, all the 1's will appear to the left of all the 0's in each row.
+
+A row i is weaker than a row j if one of the following is true:
+
+    The number of soldiers in row i is less than the number of soldiers in row j.
+    Both rows have the same number of soldiers and i < j.
+
+Return the indices of the k weakest rows in the matrix ordered from weakest to strongest.
+
+
+
+Example 1:
+
+Input: mat =
+[[1,1,0,0,0],
+ [1,1,1,1,0],
+ [1,0,0,0,0],
+ [1,1,0,0,0],
+ [1,1,1,1,1]],
+k = 3
+Output: [2,0,3]
+Explanation:
+The number of soldiers in each row is:
+- Row 0: 2
+- Row 1: 4
+- Row 2: 1
+- Row 3: 2
+- Row 4: 5
+The rows ordered from weakest to strongest are [2,0,3,1,4].
+
+Example 2:
+
+Input: mat =
+[[1,0,0,0],
+ [1,1,1,1],
+ [1,0,0,0],
+ [1,0,0,0]],
+k = 2
+Output: [0,2]
+Explanation:
+The number of soldiers in each row is:
+- Row 0: 1
+- Row 1: 4
+- Row 2: 1
+- Row 3: 1
+The rows ordered from weakest to strongest are [0,2,3,1].
+
+
+
+Constraints:
+
+    m == mat.length
+    n == mat[i].length
+    2 <= n, m <= 100
+    1 <= k <= m
+    matrix[i][j] is either 0 or 1.
+
+
+"""
+
+
+class Solution(object):
+    def kWeakestRows(self, mat, k):
+        """
+        :type mat: List[List[int]]
+        :type k: int
+        :rtype: List[int]
+
+        thought: count 1 in row of matrix, and sort by count and index
+        04/08/2022 10:15	Accepted	88 ms	13.9 MB	python
+        easy 10-20 min. learn pythonic code of sort.
+        """
+        n = len(mat)
+        lt = [[] for i in range(n)]
+        for i in range(n):
+            cnt = sum(mat[i])
+            lt[i].extend([i, cnt])
+
+        r = sorted(lt, key=lambda x: (x[1], x[0])) # sort index 1 in list of list
+        ret = list()
+        for i in range(k):
+            ret.append(r[i][0])
+        return ret
+
+
